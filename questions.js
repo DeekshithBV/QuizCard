1445cff837a1e3189fb6180b24e2905275a9dbbe
DeekshithBV <127478832+DeekshithBV@users.noreply.github.com>
2023-03-10 18:08:52 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "What does JSON stand for?",
+    answer: "Javascript object notation",
+    options: [
+      "Javascript object notation",
+      "javas orient",
+      "java sript online",
+      "jambo Multiple Language"
+    ]
+  },
+  {
+    numb: 7,
+    question: "output of int a=10;a++;cout<<a is?",
+    answer: "11",
+    options: [
+      "10",
+      "12",
+      "11",
+      "14"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does HTTP stand for?",
+    answer: "Hyper text transfer protocol",
+    options: [
+      "hper troll page",
+      "hyper titti page",
+      "Hyper text transfer protocol",
+      "none"
+    ]
+  },
+  {
+    numb: 9,
+    question: "What is size of int?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  {
+    numb: 10,
+    question: "how many types of css selector?",
+    answer: "4",
+    options: [
+      "2",
+      "3",
+      "4",
+      "1"
+    ]
+  },
+];
